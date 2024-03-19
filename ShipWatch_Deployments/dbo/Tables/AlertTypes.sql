e42d2756132f199bac9b10c4811b6ecfb7ee55f5
Omkar Mhapralkar <omkarmhapralkar@navig8group.com>
2024-03-19 15:55:21 +0530

    kk

--- ShipWatch_Deployments/dbo/Tables/AlertTypes.sql
+++ ShipWatch_Deployments/dbo/Tables/AlertTypes.sql
@@ -7,6 +7,7 @@
     [ShipwatchVisible]     BIT           DEFAULT ((0)) NOT NULL,
     [ShipwatchDescription] VARCHAR (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
     [Omkar] NCHAR(10) NULL, 
+    [Mhapralkar] NCHAR(10) NULL, 
     CONSTRAINT [Pk_AlertTypes_In] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [Uk_AlertTypes_Description] UNIQUE NONCLUSTERED ([Description] ASC)
 );
